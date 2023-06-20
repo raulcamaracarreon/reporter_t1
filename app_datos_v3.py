59a8fa1e5a37b8d46520697683b1e408129140ba
raulcamaracarreon <125494150+raulcamaracarreon@users.noreply.github.com>
2023-06-20 15:41:43 -0600

    Update app_datos_v3.py

--- app_datos_v3.py
+++ app_datos_v3.py
@@ -8,37 +8,18 @@ from io import BytesIO
 # Función para obtener el link de descarga
 def create_download_link(file_id):
     return f'https://drive.google.com/uc?id={file_id}'
-# Definir mostrar gráfica
-def show_graph(df, x_var, y_var, graph_type):
-    if graph_type == "Gráfica de barras":
-        fig = df[[x_var, y_var]].groupby(x_var).sum().plot(kind='bar').get_figure()
-    elif graph_type == "Mapa de calor":
-        fig = sns.heatmap(df[[x_var, y_var]], annot=True).get_figure()
-    elif graph_type == "Gráfica de líneas":
-        fig = df[[x_var, y_var]].plot(kind='line').get_figure()
-
-    buf = BytesIO()
-    fig.savefig(buf, format='png')
-    buf.seek(0)
-    return buf
-
 
 # IDs de tus archivos en Google Drive
 docentes_pre_id = '14dI5IetWsflUprLyURLYSC1XMWLLQ6BO'
 docentes_pri_id = '1mbV_IqFtAdYtCzbCVQSgA4rD_VViqe4M'
 docentes_sec_id = '12X7aNLjtgubaKWaKp_fmfmGplCRQAk-H'
 
-try:
-    # Cargando los datos
-    directores_url = 'https://raw.githubusercontent.com/raulcamaracarreon/reporter_t1/main/SPE2022_Dir_BD.csv'
-    directores = pd.read_csv(directores_url)
-
-    docentes_pre = pd.read_csv(create_download_link(docentes_pre_id))
-    docentes_pri = pd.read_csv(create_download_link(docentes_pri_id))
-    docentes_sec = pd.read_csv(create_download_link(docentes_sec_id))
-
-except Exception as e:
-    st.write(f"Ha ocurrido un error al cargar los datos: {e}")
+# Cargando los datos
+directores_url = 'https://raw.githubusercontent.com/raulcamaracarreon/reporter_t1/main/SPE2022_Dir_BD.csv'
+directores = pd.read_csv(directores_url)
+docentes_pre = pd.read_csv(create_download_link(docentes_pre_id))
+docentes_pri = pd.read_csv(create_download_link(docentes_pri_id))
+docentes_sec = pd.read_csv(create_download_link(docentes_sec_id))
 
 # Agregando columna de nivel educativo a cada dataframe de docentes
 docentes_pre['nivel_educativo'] = 'Preescolar'
@@ -47,6 +28,43 @@ docentes_sec['nivel_educativo'] = 'Secundaria'
 
 # Combinar todas las bases de datos de docentes
 docentes = pd.concat([docentes_pre, docentes_pri, docentes_sec])
+
+# Definir mostrar gráfica
+def show_graph(df, x_var, y_var, graph_type):
+    plt.figure(figsize=(10,6))
+    if graph_type == "Gráfica de barras":
+        if df[x_var].dtype in ['int64', 'float64'] and df[y_var].dtype in ['int64', 'float64']:
+            chart = sns.barplot(data=df, x=x_var, y=y_var)
+            for p in chart.patches:
+                chart.annotate(format(p.get_height(), '.2f'), 
+                               (p.get_x() + p.get_width() / 2., p.get_height()), 
+                               ha = 'center', va = 'center', 
+                               xytext = (0, 10), 
+                               textcoords = 'offset points')
+        else:
+            ct = pd.crosstab(df[x_var], df[y_var])
+            st.write(ct)  # Muestra el dataframe
+            ct.plot(kind="bar", stacked=True)
+    elif graph_type == "Mapa de calor":
+        ct = pd.crosstab(df[x_var], df[y_var])
+        st.write(ct)  # Muestra el dataframe
+        sns.heatmap(ct, annot=True, fmt="d")
+    elif graph_type == "Gráfica de líneas":
+        if df[x_var].dtype in ['int64', 'float64'] and df[y_var].dtype in ['int64', 'float64']:
+            sns.lineplot(data=df, x=x_var, y=y_var)
+        else:
+            ct = pd.crosstab(df[x_var], df[y_var])
+            st.write(ct)  # Muestra el dataframe
+            ct.plot(kind="line")
+
+    st.pyplot(plt.gcf())  # Agrega esta línea para mostrar la gráfica
+
+    # Guardar la gráfica en un objeto BytesIO
+    buf = BytesIO()
+    plt.savefig(buf, format="png")
+    buf.seek(0)
+
+    return buf
 
 # Opciones de roles
 role_option = st.selectbox(
@@ -67,16 +85,6 @@ else:
     else:
         df = docentes
 
-# Verificar si el DataFrame no está vacío
-if df.empty:
-    st.write("No hay datos disponibles")
-else:
-    try:
-        st.write(df.info())
-        st.write(df.head())
-    except Exception as e:
-        st.write(f"Ha ocurrido un error al visualizar los datos: {e}")
-
 x_var = st.selectbox("Selecciona la variable para el eje X", df.columns)
 y_var = st.selectbox("Selecciona la variable para el eje Y", df.columns)
 
@@ -85,11 +93,7 @@ graph_type = st.selectbox(
     ("Gráfica de barras", "Mapa de calor", "Gráfica de líneas")
 )
 
-# Asegurarse de que hay datos antes de intentar graficar
-if not df[x_var].empty and not df[y_var].empty:
-    buf = show_graph(df, x_var, y_var, graph_type)
-else:
-    st.write("No hay datos suficientes para graficar")
+buf = show_graph(df, x_var, y_var, graph_type)
 
 st.download_button(
     label="Descargar gráfica",
